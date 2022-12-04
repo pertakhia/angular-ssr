79c7e83aa191964eb0ba7a4af5310109d672d96b
pertakhia <shota.pertakhia.1@iliauni.edu.ge>
2022-12-05 01:34:56 +0400

    english word service

--- src/app/core/modules/home/home.component.ts
+++ src/app/core/modules/home/home.component.ts
@@ -1,6 +1,7 @@
 import { EnglishWordService } from './../../services/english-word.service';
 import { Observable, pipe } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { englishWordArray } from 'src/assets/word-data/english-word';
 
 @Component({
   selector: 'app-home',
@@ -8,23 +9,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public englishWordStream$: Observable<any> =
-    this.engWordService.getEnglishWord();
-  englishWordList: any[] = [];
+  public randomWordStream$: Observable<any> =
+    this.engWordService.getRandomeWord();
+
+  public englishWordStream$ = new Observable<any>();
+
+  randomWordList: any = [];
+  randomWord: string = '';
+  englishWordList: any[] = [] || 'word';
+  public translate: string = ``;
+  public wordVoice: string = ``;
+  public findAudio: any = [];
+  public wordMeaning: string = ``;
 
   constructor(private engWordService: EnglishWordService) {}
 
   ngOnInit(): void {
-    this.englishWordStream$.subscribe((data) => {
-      console.log(data);
-      this.englishWordList = data;
-    });
+    this.randomWordData();
+
+    this.otherWord();
+
+    setTimeout(() => {
+      this.translate = `https://translate.google.com/?sl=en&tl=ka&text=${this.englishWordList[0].word}&op=translate`;
+    }, 1000);
   }
 
-  otherWord() {
-    this.englishWordStream$.subscribe((data) => {
-      console.log(data);
-      this.englishWordList = data;
-    });
+  otherWord(): void {
+    this.randomWordData();
+    this.englishWordStream$ = this.engWordService
+      .getEnglishWord(this.randomWord)
+      .subscribe((data: any) => {
+        console.log(data);
+        this.wordMeaning = data[0].meanings[0].definitions[0].definition;
+        this.englishWordList = data;
+        this.findAudio = data[0].phonetics.find((item: any) => {
+          item.audio !== '';
+          return item.audio;
+        });
+        this.wordVoice = this.findAudio.audio;
+        console.log('voice', this.wordVoice);
+      });
   }
+
+  playAudio(): void {
+    const audio = new Audio();
+    audio.src = this.wordVoice;
+    audio.load();
+    audio.play();
+  }
+
+  public randomWordData(): void {
+    this.randomWord = englishWordArray[Math.floor(Math.random() * 2466)];
+  }
+
+  // public firstRandomWordPromise(): Promise<any> {
+  //   return new Promise((resolve, reject) => {
+  //     this.randomWordStream$.subscribe((data) => {
+  //       this.randomWordList = data;
+  //       this.randomWord = this.randomWordList[0];
+  //       console.log(this.randomWord);
+  //       resolve(this.randomWord);
+  //     });
+  //   }).then((data) => {
+  //     console.log(data);
+  //     this.englishWordStream$ = this.engWordService
+  //       .getEnglishWord(this.randomWord)
+  //       .subscribe((data: any) => {
+  //         console.log(data);
+  //         this.englishWordList = data;
+  //         this.findAudio = data[0].phonetics.find((item: any) => {
+  //           item.audio !== '';
+  //           return item.audio;
+  //         });
+  //         this.wordVoice = this.findAudio.audio;
+  //         console.log('voice', this.wordVoice);
+  //       });
+  //   });
+  // }
 }
--- src/app/core/services/english-word.service.ts
+++ src/app/core/services/english-word.service.ts
@@ -7,7 +7,13 @@ import { Injectable } from '@angular/core';
 export class EnglishWordService {
   constructor(private http: HttpClient) {}
 
-  getEnglishWord() {
-    return this.http.get('https://random-words-api.vercel.app/word');
+  getEnglishWord(word: string): any {
+    return this.http.get<any>(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+  }
+
+  getRandomeWord(): any {
+    return this.http.get<any>('https://random-word-api.herokuapp.com/word');
   }
 }
